a136a2bb04ac5c75f2ac26f955e4a148c7e52a4a
kanokwan112509 <sutanukanokwan@gmail.com>
2020-12-03 14:32:28 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:profile/profile.dart';
+import 'package:profile/aa.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyProfile(),
+      home: MyAa(),
     );
   }
 }
