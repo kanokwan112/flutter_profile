22fcb7419b19234d5fe66b8ec093b689d2fd3cda
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-12-03 13:33:46 +0700

    first commit

---
+++ lib/aa.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyAa extends StatelessWidget {
+  MyAa({Key key}) : super(key: key);
+
+  var sizeBox = 14.0;
+
+  TextStyle bulletStyle = GoogleFonts.prompt(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+  );
+
+  TextStyle contentBulletStyle = GoogleFonts.prompt(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  TextStyle contentStyle = GoogleFonts.kanit(
+    fontSize: 20,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Gallery'),
+      ),
+      body: SafeArea(
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: <Widget>[
+            Image.asset('assets/images/kob.jpg'),
+            Image.asset('assets/images/kk.jpg'),
+            Image.asset('assets/images/15.jpg'),
+            Image.asset('assets/images/p2.jpg'),
+            Image.asset('assets/images/k11.jpg'),
+            Image.asset('assets/images/k12.jpg'),
+            Image.asset('assets/images/17.jpg'),
+            Image.asset('assets/images/16.jpg'),
+            Image.asset('assets/images/14.jpg'),
+            Image.asset('assets/images/15.jpg'),
+            Image.asset('assets/images/12.jpg'),
+            Image.asset('assets/images/13.jpg'),
+            Image.asset('assets/images/k13.jpg'),
+            Image.asset('assets/images/p3.jpg'),
+            Image.asset('assets/images/k4.jpg'),
+            Image.asset('assets/images/k5.jpg'),
+            Image.asset('assets/images/k3.jpg'),
+            Image.asset('assets/images/k6.jpg'),
+            Image.asset('assets/images/k1.jpg'),
+            Image.asset('assets/images/k2.png'),
+            Image.asset('assets/images/1.jpg'),
+            Image.asset('assets/images/2.jpg'),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/room.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyRoom extends StatelessWidget {
+  MyRoom({Key key}) : super(key: key);
+
+  var sizeBox = 14.0;
+
+  TextStyle bulletStyle = GoogleFonts.prompt(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+  );
+
+  TextStyle contentBulletStyle = GoogleFonts.prompt(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  TextStyle contentStyle = GoogleFonts.kanit(
+    fontSize: 18,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Gallery',
+          style: contentStyle,
+        ),
+        automaticallyImplyLeading: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Center(
+                      child: Text(
+                        "My Gallery",
+                        style: bulletStyle,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/kob.jpg',
+                      height: 180.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/kk.jpg',
+                      height: 180.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "รูปกิจกรรม",
+                      style: contentBulletStyle,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/11.jpg',
+                      height: 165.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/12.jpg',
+                      height: 150.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/13.jpg',
+                      height: 150.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/14.jpg',
+                      height: 150.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/15.jpg',
+                      height: 150.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/16.jpg',
+                      height: 150.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/17.jpg',
+                      height: 150.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "รูปอาหาร",
+                      style: contentBulletStyle,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/k6.jpg',
+                      height: 175.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/k3.jpg',
+                      height: 175.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/k4.jpg',
+                      height: 175.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    child: Image.asset(
+                      'assets/images/k5.jpg',
+                      height: 175.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
